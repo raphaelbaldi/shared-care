108c1776e60dc6ab66dc9e47f73e887f9a0ed722
Raphael Baldi <raphaelbaldi@gmail.com>
2018-06-24 16:36:11 -0300

    Reorganize project as single app

--- sharedcare/settings.py
+++ sharedcare/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-m!s7657h28in3d5ty^596=%2=(1w%m!falcn@l^_mk*rbd&4='
+SECRET_KEY = '$-zv(e(om8nee-d*e$(j7ba(lyz8ianw-n*w%&!@=#w$k(&qkw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'sharedcare.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'sharedcare.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database

461ecbfc81ed7b499db4d5b8a49949ceec0ed55a
Raphael Baldi <raphaelbaldi@gmail.com>
2018-06-24 17:45:53 -0300

    Add admin site back

--- sharedcare/urls.py
+++ sharedcare/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import url, include
-from django.views.generic import TemplateView
+from django.conf.urls import url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+from django.views.generic import TemplateView
 
 from sharedcare.sharedcare import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^allergies/$', views.allergy_list, name='allergy_list'),
     url(r'^allergies/create/$', views.allergy_create, name='allergy_create'),

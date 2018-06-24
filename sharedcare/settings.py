c67ee5556915305fe12ae07c94f8a9680ff7079c
Raphael Baldi <raphaelbaldi@gmail.com>
2018-06-24 18:23:28 -0300

    Fix local settings location

--- sharedcare/settings.py
+++ sharedcare/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
 )
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError as ex:
     print("Failed to load local settings")
     print(ex)

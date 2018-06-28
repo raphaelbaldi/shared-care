5b68aa59dc4a3dce860280763337ab991c0ee651
Raphael Baldi <raphaelbaldi@gmail.com>
2018-06-27 21:59:49 -0300

    Form to allow user creating an account

--- sharedcare/sharedcare/forms.py
+++ sharedcare/sharedcare/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Allergy, Doctor, Medicine, Elderly, Food, MedicalAppointment, Meal, Prescription, ConsumedMedicine
 
@@ -59,3 +61,15 @@ class ElderlyConsumedMedicineForm(forms.ModelForm):
     class Meta:
         model = ConsumedMedicine
         fields = ('date', 'medicine', 'amount',)
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='Nome.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Sobrenome.')
+    email = forms.EmailField(max_length=254, required = True, help_text='Informe um email válido.')
+    birth_date = forms.DateField(help_text='Formato: DD/MM/YYYY')
+    cpf = forms.CharField(max_length=11, required=True, help_text="Informe seu CPF sem pontos ou traços.")
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
